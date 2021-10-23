fb2add1cabe70310bc1bc98b4b7cdc27dfd1200f
seocho <whtjdqls617@gmail.com>
2021-10-23 15:26:50 +0900

    stash 바뀌지 않은 것 다시 수정

--- frontend/srcs/Main/RecommendByAlgorithm/AskQuestion.js
+++ frontend/srcs/Main/RecommendByAlgorithm/AskQuestion.js
@@ -4,10 +4,6 @@ import { data } from "../../data/question_data";
 import { StyleSheet, Text, View } from "react-native";
 import * as Font from 'expo-font';
 
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 export const AskQuestion = ({ setData, index, updateIndex, navigation }) => {
 
   return (
@@ -20,11 +16,7 @@ export const AskQuestion = ({ setData, index, updateIndex, navigation }) => {
 			data={data[index]}
 			updateIndex={updateIndex}
 			setData={setData}
-<<<<<<< Updated upstream
-      navigation={navigation}
-=======
 			navigation={navigation}
->>>>>>> Stashed changes
       	/>
     </View>
   );
@@ -33,17 +25,12 @@ export const AskQuestion = ({ setData, index, updateIndex, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-	justifyContent : 'center'
+    justifyContent: "center",
   },
   question: {
-<<<<<<< Updated upstream
     fontFamily: "BlackHanSans_400Regular",
-    color: "#121212",
-=======
-    fontFamily: "Base",
->>>>>>> Stashed changes
     fontSize: 56,
-	marginTop : '18%',
-	marginLeft : '5%'
+    marginTop: "18%",
+    marginLeft: "5%",
   },
 });
